890dba8c34e42795ddf76df65c876f52c930c9f5
NhatAnh1801 <anh.vuong0110@hcmut.edu.vn>
2023-09-25 09:49:55 +0700

    1

--- Debug/Core/Src/LED.d
+++ Debug/Core/Src/LED.d
@@ -1,4 +1,6 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+Core/Src/LED.o: ../Core/Src/LED.c \
+ ../Core/Src/../../../Lab1_3/Core/Inc/LED.h \
+ ../Core/Src/../../../Lab1_3/Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
  ../Core/Inc/stm32f1xx_hal_conf.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
@@ -21,9 +23,11 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
- ../Core/Inc/LED.h ../Core/Inc/main.h ../Core/Inc/7SEG.h
+ ../Core/Inc/LED.h
 
-../Core/Inc/main.h:
+../Core/Src/../../../Lab1_3/Core/Inc/LED.h:
+
+../Core/Src/../../../Lab1_3/Core/Inc/main.h:
 
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 
@@ -70,7 +74,3 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
 
 ../Core/Inc/LED.h:
-
-../Core/Inc/main.h:
-
-../Core/Inc/7SEG.h:
